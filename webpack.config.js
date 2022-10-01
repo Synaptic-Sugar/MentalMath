2337b3ad867f39cb9b4a90a82df3c343672f45ed
JP Keith <jpkeith15@gmail.com>
2022-09-30 20:02:18 -0700

    Co-authored-by: ChancellorKupersmith <ChancellorKupersmith@users.noreply.github.com> Co-authored-by: forestleigh <forestleigh@users.noreply.github.com> Co-authored-by: Richard Roberts <Richard-Roberts@users.noreply.github.com> Co-authored-by: otisjones2024@outlook.com <otisjones2024@outlook.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,65 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const NODE_ENV = process.env.NODE_ENV;
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './client/index.js'),
+  },
+  mode: NODE_ENV,
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'webpack boilerplate',
+      template: '/client/index.html'
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  resolve: {
+    // Enable importing JS / JSX files without specifying their extension
+    extensions: [".js", '.jsx']
+  },
+  devServer: {
+    static: {
+      directory: './',
+    },
+    port: 8080,
+    proxy: {
+      '/api/**': {
+        'target': 'http://[::1]:3000',
+        'secure': false,
+        'changeOrigin': true
+      },
+    }
+  },
+  module:{
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          } 
+        }
+      },
+      // CSS, PostCSS, and Sass
+      {
+        test: /\.(scss|css)$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ]
+  }
+
+
+}
